50cf9a2b65fecba388f8e7d0dfef50f8c5cb301f
satojin219 <seiron1212@outlook.jp>
2021-05-14 08:20:08 +0900

    データベースに接続しました。

--- dbconnect.php
+++ dbconnect.php
@@ -4,9 +4,9 @@
 function dbConnect()
 {
 
-  $dsn = 'mysql:dbname=heroku_6b56cb56f839be0;host=us-cdbr-east-03.cleardb.com;charset=utf8';
-  $user = 'befd36e304aa83';
-  $password = 'c34364a4';
+  $dsn = 'mysql:dbname=heroku_567ac67b6b8a4e9;host=us-cdbr-east-03.cleardb.com;charset=utf8';
+  $user = 'bd4bbc72cf0720';
+  $password = '1b965628';
   $option = array(
 
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
